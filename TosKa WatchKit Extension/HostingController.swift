393f5f37fef4d9fa967b8e20051cbf1e1b2ed96f
mario armini <arminimario7@gmail.com>
2020-01-21 08:36:13 +0100

    Initial Commit

---
+++ TosKa WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  TosKa WatchKit Extension
+//
+//  Created by Mario Armini on 21/01/2020.
+//  Copyright © 2020 Mario Armini. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ TosKa WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  TosKa WatchKit Extension
+//
+//  Created by Mario Armini on 21/01/2020.
+//  Copyright © 2020 Mario Armini. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
